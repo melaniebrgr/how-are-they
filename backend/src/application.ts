2b6ca9bf37197982988cf75043bd1dc8c0d67a72
Melanie Burger <melanie.brgr@gmail.com>
2021-05-13 19:24:02 +0200

    ✨ Adds logging

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
+import * as morgan from "morgan";
 
 import {healthRouter} from "./domains/health/health.router"
 
 const app = express();
 
+app.use(morgan("tiny"));
 app.use("/health", healthRouter)
 
 export default app;
